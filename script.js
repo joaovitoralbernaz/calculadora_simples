4bf062bf9780160afcf49d160f2694e174ce3607
joaovitoralbernaz <159267082+joaovitoralbernaz@users.noreply.github.com>
2024-09-02 09:37:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function calcula (opera) {
+    let num1 = parseFloat(document.getElementById('zeroum') .value);
+    let num2 = parseFloat(document.getElementById('zerodois') .value);
+    let resultado = 0;
+}
