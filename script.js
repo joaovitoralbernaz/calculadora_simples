b4226fecae1d124d2e30f005b72402e9d00f7cbe
joaovitoralbernaz <159267082+joaovitoralbernaz@users.noreply.github.com>
2024-09-03 10:46:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ function calcula (opera) {
     let num1 = parseFloat(document.getElementById('zeroum') .value);
     let num2 = parseFloat(document.getElementById('zerodois') .value);
     let resultado = 0;
-}
+
+
+    if(opera == '+'){
+        resultado = num1 + num2;
+    }else if(opera == '-'){
+        resultado = num1 - num2;
+    }else if(opera == "*"){
+        resultado = num1 * num2;
+    }else if(opera == '/'){
+        resultado = num1 / num2;
+    }
+    document.getElementById('resultado').textContent = "resultado: " + resultado;
+    
+}    
