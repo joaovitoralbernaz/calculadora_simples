0b0757bf129c650944c93538281cde8eb300e4de
joaovitoralbernaz <159267082+joaovitoralbernaz@users.noreply.github.com>
2024-09-09 09:40:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-function calcula (opera) {
-    let num1 = parseFloat(document.getElementById('zeroum') .value);
-    let num2 = parseFloat(document.getElementById('zerodois') .value);
+function calcula() {
+    const num1 = parseFloat(document.getElementById('zeroum').value);
+    const num2 = parseFloat(document.getElementById('zerodois').value);
     let resultado = 0;
 
-
-    if(opera == '+'){
-        resultado = num1 + num2;
-    }else if(opera == '-'){
-        resultado = num1 - num2;
-    }else if(opera == "*"){
-        resultado = num1 * num2;
-    }else if(opera == '/'){
-        resultado = num1 / num2;
-    }
-    document.getElementById('resultado').textContent = "resultado: " + resultado;
+    let oper = document.querySelector('input[name="operacao"]:checked').value;
     
-}    
+    if (oper == 'soma') {
+        resultado = num1 + num2;
+    }
+    if (oper == 'subtracao') {
+        resultado = num1 - num2;
+    }
+    if (oper == 'multiplicacao') {
+        resultado = num1 * num2;
+    }
+    if (oper == 'divisao'){
+        if(num2 == 0){
+            alert?("naõ é permitido a divisão por 0");
+            re
+        }else if{
+            resultado = num1 / num2;
+        }else{
+
+        }
+
+
+
+    document.getElementById('resultado').innerText = resultado;
+
+}
